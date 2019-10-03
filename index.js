cd6c9d46222ad6750d93645012742e30d30197aa
mulekin <56100785+mulekin@users.noreply.github.com>
2019-10-02 23:49:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var tmi = require('tmi.js');
+var options = 
+{
+	options: 
+	{
+		debug: true
+	},
+	connection: 
+	{
+		cluster: "aws",
+		reconnect: true
+	},
+	identity: 
+	{
+		username: "lucasvictor97",
+		password: "oauth:3n9yvbgphk2rrmfka7ese9q65yzi5f",  
+	},
+	channels: ['#gaules', '#inr1tv', '#marcobc', '#murilo_rt', '#jeffaocs', '#pescocofino', '#csrfps', '#mibrtv', '#marcaorx', '#sheviiioficial', '#brnwowzk1', '#gabepeixe', '#stev3z', '#Vovo', '#furiatv', '#narigafps', '#stereonline', '#skipnho', '#skipnholive', '#saullo', '#mch_agg',] //ADICIONA OS CANAIS AQ COM '#NOME'
+};
+
+var client = new tmi.client(options);
+
+	client.connect();
+	client.on("connected", function (address, port)
+	{
+		console.log("address: " + address + " port: " + port);
+		client.action("999mateus", "Hello 999mateus!")
+	});
